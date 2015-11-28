85328287ff519abbda6dd2432d4e2166222af601
jahooo666 <okraskaj@ee.pw.edu.pl>
2015-11-28 12:07:06 +0100

    Initial commit

---
+++ src/Vector.java
@@ -0,0 +1,18 @@
+public class Vector {
+    Tree drzewo;
+
+    public Vector(){
+        this.drzewo = new Tree();
+    }
+
+    public void insert(int index, double value){
+        drzewo.add(index, value);
+    }
+    public double get(int index){
+        return drzewo.get(index);
+    }
+    public void printFormated(){
+        drzewo.printFormated();
+    }
+
+}
